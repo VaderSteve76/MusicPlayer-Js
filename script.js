75000fb57c5e38c73017e4288ef0c6135ed94afc
Steve <alverson.s@yahoo.com>
2020-11-10 14:06:55 -0500

    Added getElements

---
+++ script.js
@@ -0,0 +1,5 @@
+const music = document.querySelector('audio');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
