85545cec8c33fa699fdb05e367679e170a085bbd
Steve <alverson.s@yahoo.com>
2020-11-10 14:17:23 -0500

    Added play/pause functionality

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
 
+// Check if playing
+let isPlaying = false;
+
+// Play
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace('fa-play-circle', 'fa-pause');
+  playBtn.setAttribute('title', 'Pause');
+  music.play();
+}
+
+// Pause
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace('fa-pause', 'fa-play-circle');
+  playBtn.setAttribute('title', 'Play');
+  music.pause();
+}
+
+// Play || Pause
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
